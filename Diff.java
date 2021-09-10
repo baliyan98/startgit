4f0e81da46e9ae301a4fcbf8516b5b95c6616855
Vishal Baliyan <vb806133@gmail.com>
2021-09-10 19:19:54 +0530

    adding all file

---
+++ Diff.java
@@ -0,0 +1,8 @@
+package LetsLearnGit;
+
+public class Diff {
+     public static void main(String[] args) {
+         
+        System.out.println("The diff is ");
+     }
+}
--- Sum.java
+++ Sum.java
@@ -8,6 +8,6 @@ public class Sum {
         int a = 5;
         int b = 8;
         
-        System.out.println(a+b);
+        System.out.println(" the sum is " + a+b);
      }
 }
