0e77760126dc4fb34a4b5614b62e2db756154219
Vishal Baliyan <vb806133@gmail.com>
2021-09-11 16:11:07 +0530

    added multiply file

---
+++ multiply.java
@@ -0,0 +1,11 @@
+package LetsLearnGit;
+
+public class multiply {
+    
+    public static void main(String[] args) {
+        
+        int a = 3;
+        int b = 5;
+        System.out.println( a * b);
+    }
+}
